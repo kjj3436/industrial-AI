c065b153bbb44068e24e955d368e5f77c6b17923
ko jung jae <74593316+kjj3436@users.noreply.github.com>
2021-10-17 23:41:28 +0900

    Add files via upload

---
+++ projects/2021-1학기_딥러닝 실제/과제 자료/2021-03-24_image(matplotlib)/고정재.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('C:/Users/고정재/Desktop/my/kjj.png')
+
+plt.imshow(img)
+plt.show()
+
